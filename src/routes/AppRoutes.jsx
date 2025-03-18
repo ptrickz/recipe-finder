1033380c53201341a77bafcc0c78f198d3673b63
IBS Developer <ibsdeveloper@IBSs-Mac-mini.local>
2025-03-18 15:05:36 +0800

    fix: home page naming

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "../pages/NotFound";
-import Home from "../pages/home";
+import Home from "../pages/Home";
 
 const AppRoutes = () => {
   return (
